dc9884f6f1f4949be739d0076d207fa65e951e33
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-12-07 13:39:13 +1100

    Set state store correctly

--- rundocker.sh
+++ rundocker.sh
@@ -7,6 +7,8 @@ set -e
 echo Building the image
 docker build . --tag cga-kubernetes
 
+KOPS_STATE_STORE="$(ssh bosh-jumpbox.$ENV_NAME.cld.gov.au sdget kops.cld.internal state-store)"
+
 echo Starting a container with the image. It will be removed when you exit.
 docker run -it --rm \
   -v $PWD:/workspace -w /workspace \
@@ -15,6 +17,7 @@ docker run -it --rm \
   -e "AWS_REGION=ap-southeast-2" \
   -e "AWS_SDK_LOAD_CONFIG=1" \
   -e "KUBECONFIG=/workspace/.kube/${ENV_NAME}/config" \
+  -e "KOPS_STATE_STORE=${KOPS_STATE_STORE}" \
   cga-kubernetes
 
 # Setting `KUBECONFIG` to a file in the workspace is a convenience so it
